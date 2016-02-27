b881e131282a01b7a8e0127ba3226d816d028922
tws2xa <tws2xa@virginia.edu>
2016-02-27 14:01:11 -0500

    Prefabs for target indicators

---
+++ Momentum/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour
+{
+
+    public float angleOffset = 0.0f; // Angle offset in degrees.
+
+    private float angle; // Angle to mouse
+
+    /// <summary>
+    /// Use this for initialization.
+    /// </summary>
+    void Start()
+    {
+        angle = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 positionDiff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        positionDiff.Normalize();
+
+        // Get angle from 0
+        angle = Mathf.Atan2(positionDiff.y, positionDiff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + angleOffset);
+    }
+
+    /// <summary>
+    /// Get the last recorded angle towards the mouse
+    /// </summary>
+    /// <param name="includeOffset">Include the assigned offset from mouse position</param>
+    /// <returns>Angle to mouse in degrees</returns>
+    public float getAngleFromMouse(bool includeOffset)
+    {
+        return angle + (includeOffset ? 0 : angleOffset);
+    }
+}
---
+++ Momentum/Assets/Scripts/ScaleToObject.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleToObject : MonoBehaviour
+{
+
+    public GameObject target; // The object whose size to imitate.
+    public float sizeRatio = 1.0f; // Desired fraction of other objects size.
+
+    private SpriteRenderer mySpriteRenderer;
+    private SpriteRenderer targetSpriteRenderer;
+
+    private float origWidth, origHeight, origDepth;
+
+    /// <summary>
+    ///  Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        mySpriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        if (mySpriteRenderer == null)
+        {
+            Debug.LogError("Scale to Object Script Requires Sprite Renderer.");
+            origWidth = 1;
+            origHeight = 1;
+            origDepth = 1;
+        }
+        else {
+            origWidth = mySpriteRenderer.bounds.size.x;
+            origHeight = mySpriteRenderer.bounds.size.y;
+            origDepth = mySpriteRenderer.bounds.size.z;
+        }
+
+        if (target != null)
+        {
+            targetSpriteRenderer = target.GetComponent<SpriteRenderer>();
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        if (target != null && targetSpriteRenderer == null)
+        {
+            targetSpriteRenderer = target.GetComponent<SpriteRenderer>();
+        }
+        else if (target == null && targetSpriteRenderer != null)
+        {
+            targetSpriteRenderer = null;
+        }
+
+        if (target != null)
+        {
+            var targetBounds = targetSpriteRenderer.bounds;
+
+            var widthProportion = (targetBounds.size.x * sizeRatio) / origWidth;
+            var heightProportion = (targetBounds.size.y * sizeRatio) / origHeight;
+            var depthProportion = (targetBounds.size.z * sizeRatio) / origDepth;
+
+            transform.localScale = new Vector3(widthProportion, heightProportion, depthProportion);
+        }
+    }
+}
