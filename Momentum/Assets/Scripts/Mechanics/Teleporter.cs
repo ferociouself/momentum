eec3f0efc1aae306ff1e50dc15be05f73d7a90e3
ferociouself <jhe3cd@virginia.edu>
2016-02-29 20:12:23 -0500

    Added Teleporters (Prefab and Script)
    Also, I implemented World 1-1.

---
+++ Momentum/Assets/Scripts/Mechanics/Teleporter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public float teleportYChange = 10.0f;
+	public float teleportXChange = 0.0f;
+
+	private Vector2 teleportDelta;
+
+	void Start() {
+		teleportDelta = new Vector2(teleportXChange, teleportYChange);
+	}
+
+	// Called whenever another object enters the attached object's collider.
+	void OnTriggerEnter2D(Collider2D coll) {
+		coll.gameObject.transform.Translate(teleportDelta);
+	}
+}
