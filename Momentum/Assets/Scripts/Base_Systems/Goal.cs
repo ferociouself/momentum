4195f29d22540de66ba49eccf8fbee93b1212858
ferociouself <jhe3cd@virginia.edu>
2016-02-29 23:10:19 -0500

    Removed Userprefs

---
+++ Momentum/Assets/Scripts/Base_Systems/Goal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	private bool active = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+			// Pull the Player in here
+			// After a timeframe, or once the player is in the center, end the level.
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		active = true;
+	}
+
+	private bool EndLevel() {
+		return false;
+	}
+}
