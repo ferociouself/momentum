72ac6225e34cb8d26730f20f6ebf8a92c46f799f
ferociouself <jhe3cd@virginia.edu>
2016-03-01 09:47:17 -0500

    Goal Switches Levels
    When you enter the goal, it switches levels (this is purely arbitrary
    right now, it always switches to World1_2).

--- Momentum/Assets/Scripts/Base_Systems/Goal.cs
+++ Momentum/Assets/Scripts/Base_Systems/Goal.cs
@@ -14,15 +14,23 @@ public class Goal : MonoBehaviour {
 	void Update () {
 		if (active) {
 			// Pull the Player in here
+
+
+
 			// After a timeframe, or once the player is in the center, end the level.
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		active = true;
+		EndLevel();
 	}
 
 	private bool EndLevel() {
-		return false;
+		UnityEngine.SceneManagement.SceneManager.LoadScene("World1_2");
+		UnityEngine.SceneManagement.SceneManager.SetActiveScene(
+			UnityEngine.SceneManagement.SceneManager.GetSceneByName("World1_2"));
+		Debug.Log("Ending Level");
+		return true;
 	}
 }
