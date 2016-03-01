3be15911142dfc3c1b80b068dd85dd30bc8f5026
zsd4yr <Sage@Zacharys-MacBook-Pro.local>
2016-02-29 22:07:32 -0500

    UI template stuff
    Set up a dummy script for now. Would love Thomas’ help next meeting
    implementing something like this. Way out of my depth here. Also made
    the buttons display the right text…
    
    Maybe we could get some cool button prefabs?
    
    OH! and realized I accidentally called it ShowHub instead of ShowHud
    but I thought that was funny so now it’s ShowHubby

---
+++ Momentum/Assets/Scripts/ShowHubby.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHud {
+	private EditModeScript editModeControllerScript;
+
+	void Start(){
+		//editModeControllerScript = FindObjectOfType<EditModeScript>();
+	}
+
+	void Update(){
+		//check to see if in edit mode
+		//if in edit mode, show canvas nice and slow like
+		//if not, don't.
+	}
+
+}
+
+
