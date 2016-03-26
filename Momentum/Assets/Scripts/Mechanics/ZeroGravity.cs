076d111d85545d7e187e12fe47ca9910fecf7729
d-wire <ljd3za@virginia.edu>
2016-03-26 17:58:22 -0400

    Added Zero Gravity Script
    Created the zero gravity script.

---
+++ Momentum/Assets/Scripts/Mechanics/ZeroGravity.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZeroGravity : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+        GameObject[] allObjs = GameObject.FindGameObjectsWithTag("Circle");
+        foreach (GameObject obj in allObjs)
+        {
+            Vector3 pos = obj.transform.position;
+            Debug.Log("In Loop: " + pos);
+            if (PointInOABB(pos, gameObject.GetComponent<BoxCollider2D>()))
+            {
+                obj.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        coll.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    public void OnTriggerExit2D(Collider2D coll)
+    {
+        coll.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    bool PointInOABB(Vector3 point, BoxCollider2D box)
+    {
+        Vector3 inverse = box.transform.InverseTransformPoint(point);
+        point = new Vector2(inverse.x, inverse.y) - box.offset;
+
+        float halfX = (box.size.x * 0.5f);
+        float halfY = (box.size.y * 0.5f);
+        if (point.x < halfX && point.x > -halfX &&
+           point.y < halfY && point.y > -halfY)
+            return true;
+        else
+            return false;
+    }
+}
+
