63d9442dd3a477775e6e3e3526716f98f35b3ba6
tws2xa <tws2xa@virginia.edu>
2016-04-02 02:50:25 -0400

    Alternate Appearance for Gravity Warpers

---
+++ Momentum/Assets/Scripts/AutoSpin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSpin : MonoBehaviour {
+
+    public float xSpinSpeed = 0; // Degrees per second.
+    public float ySpinSpeed = 0; // Degrees per second.
+    public float zSpinSpeed = 180.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Fixed Update is called in fixed time intervals
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(new Vector3(xSpinSpeed * Time.deltaTime, ySpinSpeed * Time.deltaTime, zSpinSpeed * Time.deltaTime));
+    }
+}
