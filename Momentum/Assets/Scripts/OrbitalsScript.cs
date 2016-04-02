a25c257fad763765841a3ad8832cf00f1fa9835a
tws2xa <tws2xa@virginia.edu>
2016-04-02 02:50:25 -0400

    Alternate Appearance for Gravity Warpers

---
+++ Momentum/Assets/Scripts/OrbitalsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrbitalsScript : MonoBehaviour {
+
+    public float orbitalSpeed = 180.0f; // Degrees per second
+    public List<GameObject> orbitals; // Horizontal Orbitals
+
+    private Vector3 rotationAxis;
+
+	// Use this for initialization
+	void Start () {
+        rotationAxis = new Vector3(0, 0, 1); // Rotate around z axis.
+	}
+	
+	// Fixed Update is called in fixed time intervals
+	void FixedUpdate () {
+        foreach(GameObject orbital in orbitals) {
+            orbital.transform.RotateAround(transform.position, rotationAxis, orbitalSpeed * Time.deltaTime);
+        }
+    }
+}
