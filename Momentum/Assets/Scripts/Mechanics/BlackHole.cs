afcd607b4d9027a1cc35e9920f34bdd73f3f942d
tws2xa <tws2xa@virginia.edu>
2016-04-02 17:36:35 -0400

    Updated Gravity Warper (Blackhole)

--- Momentum/Assets/Scripts/Mechanics/BlackHole.cs
+++ Momentum/Assets/Scripts/Mechanics/BlackHole.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class BlackHole : MonoBehaviour {
 
 	public float pullValue;
+    public float minAlpha = 0;
+    public float maxAlpha = 1;
 
 	Vector2 pullForce;
 
@@ -26,9 +28,15 @@ public class BlackHole : MonoBehaviour {
 			SpriteRenderer objectColor = playerObj.GetComponent<SpriteRenderer>();
 
 			float dist = distance(gameObject.transform, playerObj.transform);
+            float maxDist = gameObject.GetComponent<CircleCollider2D>().radius * 5;
+            float newAlpha = minAlpha + ((maxAlpha - minAlpha) * Mathf.Min(1, dist / maxDist));
 
-			objectColor.color = new Color(objectColor.color.r, objectColor.color.g, objectColor.color.b, 
-				dist/(gameObject.GetComponent<CircleCollider2D>().radius * 5));
+            objectColor.color = new Color(
+                objectColor.color.r,
+                objectColor.color.g,
+                objectColor.color.b,
+                newAlpha
+                );
 
 			rb.AddForce(pullForce * pullValue);
 		} else {
@@ -53,8 +61,8 @@ public class BlackHole : MonoBehaviour {
 	}
 
 	private float distance(Transform t1, Transform t2) {
-		//gameObject.transform.localPosition.x - playerObj.transform.localPosition.x, 
-		//	gameObject.transform.localPosition.y - playerObj.transform.localPosition.y;
+		// gameObject.transform.localPosition.x - playerObj.transform.localPosition.x, 
+		// gameObject.transform.localPosition.y - playerObj.transform.localPosition.y;
 		return Mathf.Sqrt(Mathf.Pow(t1.localPosition.x - t2.localPosition.x, 2) + Mathf.Pow(t1.localPosition.y - t2.localPosition.y, 2));
 	}
 }
