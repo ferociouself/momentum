08eeae6df4d9f038f2de033772a8566e1cca35f8
ferociouself <jhe3cd@virginia.edu>
2016-04-12 13:10:41 -0400

    Added "Hard Repulsor" and "Bouncy Floor"

---
+++ Momentum/Assets/Scripts/Mechanics/HardRepulsor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HardRepulsor : MonoBehaviour {
+
+	private Vector2 pushForce;
+	private float pushMagnitude;
+
+	public float pushModifier;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log(coll.rigidbody.velocity.magnitude);
+		pushForce = new Vector2(coll.transform.position.x - gameObject.transform.position.x, 
+			coll.transform.position.y - gameObject.transform.position.y);
+		pushMagnitude = coll.rigidbody.velocity.magnitude * pushModifier;
+		pushForce = pushForce * pushMagnitude;
+		coll.gameObject.GetComponent<Rigidbody2D>().AddForce(pushForce);
+	}
+}
