ffaf14a43dec7208e53b6eb9ebc422b3339a6f96
zsd4yr <zsd4yr@virginia.edu>
2016-04-16 17:31:52 -0400

    have you pushed yet
    YOU THOUGHT YOU DID! THAT’S NOWHERE NIER GOOD ENOUGH@

--- Momentum/Assets/Scripts/Base_Systems/PauseConnector.cs
+++ Momentum/Assets/Scripts/Base_Systems/PauseConnector.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PauseConnector : MonoBehaviour {
 
+	private PauseResourceController pauseCont;
+	private float pausability;
 	// Use this for initialization
 	void Start () {
-	
+		pauseCont = GameObject.Find ("EditModeController").GetComponent<PauseResourceController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		pausability = pauseCont.getPauseResource ();
+		gameObject.GetComponent<Slider> ().value = pausability;
 	}
 }
