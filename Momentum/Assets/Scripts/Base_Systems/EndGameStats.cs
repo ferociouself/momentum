ca99bd99dff86fb179565bdd788d1e1a13e22db1
ferociouself <jhe3cd@virginia.edu>
2016-04-30 16:39:49 -0400

    Wow end game stats
    Wow nothing interacts with it yet

---
+++ Momentum/Assets/Scripts/Base_Systems/EndGameStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameStats {
+	public static int totalPauseCount;
+	public static long initTime;
+	public static long endTime;
+	public static long totalPauseTime;
+
+	public static int numRestarts;
+
+	public static string hardestLevel;
+	public static Image hardestLevelImage;
+	public static int maxRestarts;
+
+	public static void updateHardestLevel() {
+
+	}
+}
